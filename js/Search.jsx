edf55c41fe16ffd15e924462a447c60c21deb087
Jake Perez <jacobanthony.perez@gmail.com>
2018-03-29 01:06:22 -0700

    add plugin to allow for arrow functions in react methods

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = props => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard show={show} key={show.imdbID} />)}
-    </div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: "this is a test term"
+  };
+
+  handleSearchTermChange = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>netflicks</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="search"
+          />
+        </header>
+
+        <div>
+          {preload.shows.map(show => (
+            <ShowCard show={show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
