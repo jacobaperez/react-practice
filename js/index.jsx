011317f1e17668d8682382c26e79a74eb65b3269
Jake Perez <jacobanthony.perez@gmail.com>
2018-03-29 18:25:24 -0700

    complete app without redux

--- js/index.jsx
+++ js/index.jsx
@@ -1,17 +1,22 @@
 // @flow
 
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import Perf from "react-addons-perf";
+
+// only for development, delete when shipping production
+window.Perf = Perf;
+Perf.start();
 
 const renderApp = () => {
-  render(<App />, document.getElementById('app'));
+  render(<App />, document.getElementById("app"));
 };
 
 renderApp();
 
 if (module.hot) {
-  module.hot.accept('./App', () => {
+  module.hot.accept("./App", () => {
     renderApp();
   });
 }
